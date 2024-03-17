4017d431e1acfcc4a3360b029862ac9e5f611a36
rejabagja <rezasubagja5@gmail.com>
2024-03-17 11:52:03 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit cupiditate eaque molestias numquam assumenda tenetur asperiores, perferendis saepe consequuntur nulla?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
